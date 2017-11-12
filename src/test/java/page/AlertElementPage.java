2df55cca492b2439dceb6f05d2c704d39ddff0c5
egor.kartushin <egor.kartushin@simbirsoft.com>
2017-11-12 20:02:07 +0400

    removed builder

--- src/test/java/page/AlertElementPage.java
+++ src/test/java/page/AlertElementPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,10 +14,8 @@ public class AlertElementPage {
     private WebElement demonstrateAlertButton;
     private WebElement resultString;
     private WebDriverWait wait;
-    private Actions builder;
 
     public AlertElementPage(WebDriver driver) {
-        builder = new Actions(driver);
         wait = new WebDriverWait(driver, 25);
         wait.until(ExpectedConditions.visibilityOf(
                 driver.findElement(By.xpath("//a[contains(.,'Input Alert')]"))));
